00955b0340eb989e4bf5916b11932af243c86920
Waffai-Developer <73185721+Waffai@users.noreply.github.com>
2023-03-25 23:34:12 +0800

    record and body

--- 3_upload_assets_record.py
+++ 3_upload_assets_record.py
@@ -242,13 +242,13 @@ def modify_record(record_name, asset_dict):
         "recordType": question["recordType"],
         "fields": {
             "question": {"value": question_fields["question"]},
-            # "difficulty": {"value": question_fields["difficulty"]},
-            # "chinese": {"value": question_fields["chinese"]},
-            # "germany": {"value": question_fields["germany"]},
-            # "japanese": {"value": question_fields["japanese"]},
-            # "germanyAcademicVocabulary": {"value": question_fields["germanyAcademicVocabulary"]},
-            # "japaneseAcademicVocabulary": {"value": question_fields["japaneseAcademicVocabulary"]},
-            # "academicVocabulary": {"value": question_fields["academicVocabulary"]},
+            "difficulty": {"value": question_fields["difficulty"]},
+            "chinese": {"value": question_fields["chinese"]},
+            "germany": {"value": question_fields["germany"]},
+            "japanese": {"value": question_fields["japanese"]},
+            "germanyAcademicVocabulary": {"value": question_fields["germanyAcademicVocabulary"]},
+            "japaneseAcademicVocabulary": {"value": question_fields["japaneseAcademicVocabulary"]},
+            "academicVocabulary": {"value": question_fields["academicVocabulary"]},
             "audio": {"value": asset}
         }
     }
