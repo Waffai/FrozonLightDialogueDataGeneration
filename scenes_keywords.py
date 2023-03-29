0473fe3285521332773e1fb7a38b5ab0c7c9053c
Waffai-Developer <73185721+Waffai@users.noreply.github.com>
2023-03-30 00:11:31 +0800

    step1-developing

---
+++ scenes_keywords.py
@@ -0,0 +1,13 @@
+
+scenes_keywords = {
+    "airport": ["flight", "boarding pass", "passport", "security check", "luggage", "terminal"],
+    "cafe": ["coffee", "latte", "cappuccino", "pastry", "muffin", "sandwich"],
+    "restaurant": ["reservation", "table", "menu", "appetizer", "entree", "dessert"],
+    "hotel": ["reservation", "check-in", "check-out", "room service", "amenities", "housekeeping"],
+    "shopping": ["sale", "discount", "cashier", "refund", "receipt", "shopping bag"],
+    "bank": ["account", "deposit", "withdrawal", "loan", "ATM", "teller"],
+    "hospital": ["appointment", "doctor", "nurse", "emergency room", "medicine", "insurance"],
+    "school": ["enrollment", "registration", "tuition", "classroom", "textbook", "professor"],
+    "movie theater": ["ticket", "popcorn", "soda", "screening", "seating", "rating"],
+    "gym": ["membership", "trainer", "equipment", "workout", "locker room", "sauna"]
+}
